ea5bbb24214f39155ea64c9f871803cf06378eb2
RameshSTHA <shrestha.ramesh000@gmail.com>
2022-06-01 19:45:47 +1000

    first file  is created

---
+++ first.py
@@ -0,0 +1,13 @@
+# the national anthem in the things are changed according to the developent o the fot he things right
+import math
+
+
+def rameshDevelopment():
+    print("THE DEVELOPMENT IN THE ASIA")
+    firstNum = int(input("Enter the positive integer Numebr: "))
+    if firstNum > 0:
+        factorial_number = math.factorial(firstNum)
+    if factorial_number >= 900:
+        print("it is accessible for the developnent of the maangement in the sense ")
+    else:
+        print("yoy need to increase  the numebr for more developemnt")
